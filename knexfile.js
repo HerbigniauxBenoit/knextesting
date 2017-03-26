dd6f782467abbcf7390ea5efd253a9a0c9910ae6
HerbigniauxBenoit <b.herbigniaux@hotmail.fr>
2017-03-26 23:30:04 +0200

    Initial commit for my first knex.js project

---
+++ knexfile.js
@@ -0,0 +1,57 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+      client: 'mysql',
+      connection: {
+          host: '127.0.0.1',
+          user: 'root',
+          password: '',
+          database: 'knextest',
+          charset: 'utf8'
+      }
+  },
+    benoit: {
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'knex',
+            charset: 'utf8'
+        }
+    },
+
+  //staging: {
+  //  client: 'postgresql',
+  //  connection: {
+  //    database: 'my_db',
+  //    user:     'username',
+  //    password: 'password'
+  //  },
+  //  pool: {
+  //    min: 2,
+  //    max: 10
+  //  },
+  //  migrations: {
+  //    tableName: 'knex_migrations'
+  //  }
+  //},
+
+  //production: {
+  //  client: 'postgresql',
+  //  connection: {
+  //    database: 'my_db',
+  //    user:     'username',
+  //    password: 'password'
+  //  },
+  //  pool: {
+  //    min: 2,
+  //    max: 10
+  //  },
+  //  migrations: {
+  //    tableName: 'knex_migrations'
+  //  }
+  //}
+
+};
